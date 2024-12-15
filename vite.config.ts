a84df5c60cb5fec7868031d72f9a9acf1b597c0a
Seif Boukerdenna <elmelz6472@gmail.com>
2024-12-15 16:16:26 -0500

    added connect/disconnect for solana wallet

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...(nodePolyfills() as unknown as Plugin),
+      enforce: "post",
+    },
+  ],
+  resolve: {
+    alias: {
+      crypto: "rollup-plugin-node-polyfills/polyfills/crypto-browserify",
+    },
+  },
 });
