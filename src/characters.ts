ae4869b1c28d8e31ab189477223bc135e8be86eb
Seif Boukerdenna <elmelz6472@gmail.com>
2024-12-16 21:34:53 -0500

    fixed build error

--- src/characters.ts
+++
@@ -1,9 +0,0 @@
-import { Character } from "./components/CharacterCarousel";
-
-const characters: Character[] = [
-  { id: 1, name: "Aristotle", img: "/src/assets/aristotle.jpg" },
-  { id: 2, name: "Cleopatra", img: "/src/assets/cleopatra.jpg" },
-  { id: 3, name: "Leonardo da Vinci", img: "/src/assets/da_vinci.jpg" },
-];
-
-export default characters;
