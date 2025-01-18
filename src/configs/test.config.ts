5df22f8627451be1126ef8e297a8c9bd507e0fbe
Seif Boukerdenna <elmelz6472@gmail.com>
2025-01-17 21:56:55 -0500

    disabled challenge view

--- src/configs/test.config.ts
+++ src/configs/test.config.ts
@@ -3,6 +3,6 @@
 export const TEST_MODE = true; // Toggle this for testing
 export const FREE_CHARACTER_ID = "1"; // Trump's ID
 
-export const CHALLENGES_ENABLED = true;
+export const CHALLENGES_ENABLED = false;
 
 export const SHOW_CONTRACT_HASH = false;
